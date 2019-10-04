ef5d7e7ac02eef810c0aef32a9d32040a74fa750
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-10-04 17:34:33 +0530

    conflict resolved

--- modules/rsalesorder/rsalesorder.js
+++ modules/rsalesorder/rsalesorder.js
@@ -13,20 +13,12 @@ var rSalesOrder=(function(){
     function rSalesOrder(xmljs){
         BaseModule.call(this,xmljs);
     };
-<<<<<<< HEAD
     rSalesOrder.prototype.import=function(xml){
-        
-        this.saveXml(xml);
-=======
-    rSalesOrder.prototype.import=function(){
-        var crdr=new CollecReader(this._xmljs);
-        // crdr.lineItems();
->>>>>>> 897266d72e6bb90b21da91deb100f848d6a36379
         const dbconn=this.getDb();
         const CrmEntity=dbconn.import('./../../models/crmentity');
-        CrmEntity.findOne().then(entity => {
-            console.log(entity.dataValues);
-        });
+        var crdr=new CollecReader(this._xmljs);
+        crdr.lineItems();
+        return this.saveXml(xml);
     };
     return rSalesOrder;
 })();
