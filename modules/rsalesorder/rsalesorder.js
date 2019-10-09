9f6afd2255efcb12c32208216531f59f2e8fc2b2
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-10-09 10:07:41 +0530

    ability to associate models inside module before using them has been done.

--- modules/rsalesorder/rsalesorder.js
+++ modules/rsalesorder/rsalesorder.js
@@ -13,9 +13,58 @@ var rSalesOrder=(function(){
     function rSalesOrder(xmljs){
         BaseModule.call(this,xmljs);
     };
-    rSalesOrder.prototype.import=function(xml){
+    rSalesOrder.prototype.importAssoc=function(){
         const dbconn=this.getDb();
         const CrmEntity=dbconn.import('./../../models/crmentity');
+        const rSalesOrder=dbconn.import('./../../models/rsalesorder');
+        const rSalesOrderCf=dbconn.import('./../../models/rsalesorder-cf');
+        const rSoProductRel=dbconn.import('./../../models/rso-productrel');
+        rSalesOrder.belongsTo(CrmEntity,{
+            as:'Crm',
+            foreignKey:'salesorderid',
+            targetKey:'crmid',
+        });
+        rSalesOrder.hasOne(rSalesOrderCf,{
+            foreignKey:'salesorderid',
+            sourceKey:'salesorderid',
+            as:'Cf'
+        });
+        rSalesOrder.hasMany(rSoProductRel,{
+            foreignKey:'id',
+            sourceKey:'salesorderid',
+            as:'Rel'
+        });
+        this.models['CrmEntity']=CrmEntity;
+        this.models['rSalesOrder']=rSalesOrder;
+        this.models['rSalesOrderCf']=rSalesOrderCf;
+        this.models['rSoProductRel']=rSoProductRel;
+        return this;
+    };
+    rSalesOrder.prototype.import=function(xml){
+        this.importAssoc();
+        var rSalesOrder=this.models['rSalesOrder'];
+        var p$=rSalesOrder.findOne()
+        // get mapped crmentity using sequelize belongsTo association
+        p$.then(rso => {
+            console.log(rso.dataValues);
+            return rso.getCrm();
+        })
+        .then(crme => {
+            console.log(crme.dataValues);
+        });
+        // get mapped productrel using sequelize hasMany association
+        p$.then(rso => {
+            return rso.getRel();
+        })
+        .then(rels =>{
+            rels.forEach(rel => console.log(rel.dataValues));
+        });
+        // get mapped cf table using sequelize hasOne association
+        p$.then(rso => {
+            return rso.getCf();
+        })
+        .then(rsocf => console.log(rsocf.dataValues));
+        // use custom collection reader to read xml js 
         var crdr=new CollecReader(this._xmljs);
         crdr.lineItems();
         return this.saveXml(xml);
