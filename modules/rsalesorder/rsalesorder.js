eadc5decc2586182a24df9547e4ee3cfc2f3b12c
peddaswamy <kpswamy540@gmail.com>
2020-01-28 18:46:50 +0530

    dd

--- modules/rsalesorder/rsalesorder.js
+++ modules/rsalesorder/rsalesorder.js
@@ -160,6 +160,7 @@ const Op = Sequelize.Op
  			 	 //default related module for buyerid is xRetailer
  			 		switch(field.columnname){
  			 			case 'buyerid':
+ 			 			console.log(coll.customer_type._text);
  			 			var buyerid=await self.getBuyerId(coll.customer_type._text,coll);
  			 			console.log(buyerid);
  			 			if(buyerid){
