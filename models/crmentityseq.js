109702cebde68f668b455d6ff86a44edc0ed77cd
peddaswamy <kpswamy540@gmail.com>
2020-08-13 17:24:18 +0530

    seq log

--- models/crmentityseq.js
+++ models/crmentityseq.js
@@ -19,6 +19,8 @@ module.exports=(sequelize,DataTypes)=>{
             }
         }).spread( result => {
             return result.insertId;
+        }).catch(e=>{
+            log.error("vtiger_crmentity_seq error "+e.message)
         });
     
     }
