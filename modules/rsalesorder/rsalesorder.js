7f7ccda9153b96915307c96c5f1de38f7f3b4fcc
peddaswamy <kpswamy540@gmail.com>
2020-08-17 15:04:58 +0530

    series

--- modules/rsalesorder/rsalesorder.js
+++ modules/rsalesorder/rsalesorder.js
@@ -2318,7 +2318,7 @@ rSalesOrder.prototype.getFields=async function (log){
  						const dbconn=this.getDb();
  						const XSeries=dbconn.import('./../../models/x-series');
  						const XSeriesCf=dbconn.import('./../../models/x-series-cf');
- 						return XSeriesCf.findOne({
+ 						return await XSeriesCf.findOne({
  							where:{
  								cf_xtransactionseries_transaction_type:type
  							},
